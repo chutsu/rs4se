3651cfeade0ca637a4b3beb0325ca0ad5999b36e
Chris Choi <chutsu@gmail.com>
2022-01-11 11:12:37 +0000

    Update run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
+ctags -R .
+
+debug() {
+  gdb \
+    -ex=run \
+    -ex=bt \
+    -ex="set confirm off" \
+    -ex=quit \
+    --args "$1" "$2"
+}
 
 # sudo bash scripts/install_deps/run.bash
 bash scripts/format_code.bash
@@ -8,10 +18,11 @@ cd ~/catkin_ws
 source /opt/ros/melodic/setup.bash
 source devel/setup.bash
 # catkin clean
-catkin build rs4se -DCMAKE_BUILD_TYPE=Release -j2
-# catkin build rs4se -DCMAKE_BUILD_TYPE=Debug -j2
-roslaunch rs4se intel_d435i.launch
-# roslaunch rs4se intel_d435i-intrinsics_calib.launch
+# catkin build rs4se -DCMAKE_BUILD_TYPE=Release -j2
+catkin build rs4se -DCMAKE_BUILD_TYPE=Debug -j2
+# roslaunch rs4se intel_d435i.launch
 
-# rosrun rs4se test_rs4se
-# rosrun --prefix 'gdb -ex run' rs4se test_rs
+# rosrun --prefix 'valgrind --leak-check=full' rs4se test_rs4se
+# rosrun --prefix "gdb -ex=run -ex=bt -ex=quit" rs4se test_rs4se
+
+rosrun rs4se test_rs4se
